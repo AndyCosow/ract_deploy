5f7f90a53db723eaf7398c32ff471e8fc70e1a34
Andrius Kasovskis <andrius@Andriuss-MacBook-Air.local>
2022-08-30 10:47:22 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+const remoteIp = "159.89.8.148"
 
 function App() {
+
+  const [num, setNum] = useState(0)
+
+  const getData = async () => {
+
+    const res = await fetch(`http://${remoteIp}:8000/info`)
+    const data = await res.json()
+
+    setNum(data.number)
+
+    console.log(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Random number: {num}</h1>
+
+      <button onClick={getData}>Get Data</button>
     </div>
   );
 }
